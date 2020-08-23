1a504554b52e55cfdb89352c6194040b65bc0f67
Glitch (cogitable) <none>
2020-08-23 21:16:19 +0000

    Add Issue Creation Event

--- index.js
+++ index.js
@@ -1,62 +1,76 @@
 module.exports = app => {
+  app.on(
+    ["issues.opened", "issue_comment.created", "issue_comment.edited"],
+    async context => {
+      let body = context.payload.comment.body;
+      var reg = /\[\[(.+?)\]\]/g;
+      var links = [];
+      var owner = context.repo().owner;
+      var repo = context.repo().repo;
+      var onwer_repo = owner + "/" + repo;
 
-  app.on(['issue.created','issue_comment.created', 'issue_comment.edited' ], async context => {
+      var current_issue = await context.github.issues.get(
+        context.repo({
+          issue_number: context.issue().number
+        })
+      );
 
-    let body = context.payload.comment.body
-    var reg = /\[\[(.+?)\]\]/g  
-    var links = []
-    var owner = context.repo().owner
-    var repo = context.repo().repo
-    var onwer_repo = owner + '/' + repo
-
-    var current_issue = await context.github.issues.get(context.repo({
-        issue_number: context.issue().number 
-    }))
-
-    if (body.match(/\[\[(.+?)\]\]/g)){
-
-        var match
-        while((match = reg.exec(body)) !== null) {
-            const link = match[1]
-            context.github.search
-            const results = await context.github.search.issuesAndPullRequests(context.repo({
-
-                q: link + `repo:${onwer_repo}`,
-                order: 'asc',
-                per_page: 1
-            }))
-            const items = results.data.items
-            if (typeof items !== undefined && typeof items[0] !== undefined){
-                var result_number = items[0].number
-                var result_title = items[0].title 
-            }else{
-                var result_title = null
-            }
-            let link_number
-            if( link === result_title ){
-                link_number = result_number
-            }else{
-                let new_issue = await context.github.issues.create(context.repo({
-                    title: link
-                }))
-                link_number = new_issue.data.number
-            }
-            links.push([link, link_number])
+      if (body.match(/\[\[(.+?)\]\]/g)) {
+        var match;
+        while ((match = reg.exec(body)) !== null) {
+          const link = match[1];
+          context.github.search;
+          const results = await context.github.search.issuesAndPullRequests(
+            context.repo({
+              q: link + `repo:${onwer_repo}`,
+              order: "asc",
+              per_page: 1
+            })
+          );
+          const items = results.data.items;
+          if (typeof items !== undefined && typeof items[0] !== undefined) {
+            var result_number = items[0].number;
+            var result_title = items[0].title;
+          } else {
+            var result_title = null;
+          }
+          let link_number;
+          if (link === result_title) {
+            link_number = result_number;
+          } else {
+            let new_issue = await context.github.issues.create(
+              context.repo({
+                title: link
+              })
+            );
+            link_number = new_issue.data.number;
+          }
+          links.push([link, link_number]);
         }
-    }else{
-        app.log('No Matchs')
+      } else {
+        app.log("No Matchs");
+      }
+      links.forEach(
+        link =>
+          (body = body.replace(`[[${link[0]}]]`, `[${link[0]}](${link[1]})`))
+      );
+
+      links.forEach(link =>
+        context.github.issues.createComment(
+          context.repo({
+            issue_number: link[1],
+            body:
+              `Cited on [${current_issue.data.title}](${current_issue.data.number}#issuecomment-${context.payload.comment.id})  \n > ` +
+              body
+          })
+        )
+      );
+      await context.github.issues.updateComment(
+        context.repo({
+          comment_id: context.payload.comment.id,
+          body: body
+        })
+      );
     }
-    links.forEach(link => body = body.replace(`[[${link[0]}]]`, `[${link[0]}](${link[1]})`))
-
-    links.forEach(( link => context.github.issues.createComment(context.repo({
-        issue_number: link[1],
-        body: `Cited on [${current_issue.data.title}](${current_issue.data.number}#issuecomment-${context.payload.comment.id})  \n > `  + body 
-      }))
-    ))
-    await context.github.issues.updateComment(context.repo({
-        comment_id: context.payload.comment.id, 
-        body: body
-    }))
-
-  })
-}
+  );
+};
